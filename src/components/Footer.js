42addd4a5d699d9dd742fe81f916850c6bf7af61
Himanshu Kushwaha <hk5652990@gmail.com>
2025-04-12 14:52:31 +0530

    third project add

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer" data-aos="fade-up">
-      <p>&copy; {new Date().getFullYear()} Your Name. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} Himanshu Kushwaha. All rights reserved.</p>
       <p>Built with React.</p>
     </footer>
   );
